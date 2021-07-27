acd8dfd1855b2985ed6712bc3fd3b20a2eaa38fd
JERIN 3J• <87818715+Jerin3j@users.noreply.github.com>
2021-07-27 18:17:17 +0530

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,7 +1,7 @@
 function handler(m) {
   // Ini cuma contoh, jgn di uncomment -_-
-  // F this.sendContact(m.chat, '919526941079', 'Jerin3j', m)
-  this.sendContact(m.chat, '919526941079', 'Jerin3j, m)
+  // F this.sendContact(m.chat,'919526941079', 'Jerin3j', m)
+  this.sendContact(m.chat,'919526941079', 'Jerin3j, m)
 }
 handler.help = ['owner', 'creator']
 handler.tags = ['info']
